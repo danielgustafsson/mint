981031fde3fc2bdb4798af1e5efb8ae13f603283
Daniel Gustafsson <daniel@yesql.se>
2016-03-25 12:16:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mint Unit
+
+A minimal unit testing framework for C code. See comment in `mintunit.h` for
+sample usage.
---
+++ mintunit.h
@@ -0,0 +1,81 @@
+/*
+ * mintunit.h
+ *	Mint Unit -- a minimal unit testing framework for C
+ *
+ * Mint Unit has a TAP like output syntax but unlike TAP implementations it
+ * stops on the first failing test following the fail-fast fail-hard idea of
+ * being forced to fix tests rather than observe broken ones. This code was
+ * influenced by http://www.jera.com/techinfo/jtns/jtn002.html
+ *
+ * Sample usage of mint unit can be seen below:
+ *
+ *	#include <stdio.h>
+ *	#include "mintunit.h"
+ *
+ *	static char *
+ *	test_foo(void)
+ *	{
+ *		 mt_assert("Test foo function", foo(5) == 5);
+ *		 mt_is_null("Test bar function returns NULL", bar(5));
+ *		 return NULL;
+ *	}
+ *
+ *	static char *
+ *	run_tests(void)
+ *	{
+ *		mt_init(1);
+ *		mt_run_test("Testing foo", test_foo);
+ *		return NULL;
+ *	}
+ *
+ *	int
+ *	main(int argc, char **argv)
+ *	{
+ *		char *test_result = run_tests();
+ *		if (!test_result)
+ *			printf("\n# All tests passed\n");
+ *		printf("# Test Suites run: %d\n# Unit Tests run : %d\n", tests_run, units_run);
+ *
+ *		return test_result == NULL ? 0 : 1;
+ *	}
+ *
+ * Portions Copyright (c) 2016 Daniel Gustafsson <daniel@yesql.se>
+ * Portions copyright (c) 2002 Jera Design
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef _MINTUNIT_H
+#define _MINTUNIT_H
+
+unsigned int tests_run = 0;
+unsigned int units_run = 0;
+
+/* mt_init - Initalize with the number of suites */
+#define mt_init(tests) do { printf("1..%d\n", tests); } while (0)
+/* mt_assert - General purpose assertion */
+#define mt_assert(message, test) do { units_run++; if (!(test)) return message; } while (0)
+/* mt_is_null - Specifically test for NULL */
+#define mt_is_null(message, test) do { units_run++; if ((test) != NULL) return message; } while (0)
+/* mt_run_test - Run a test suite */
+#define mt_run_test(message, test) do { char *result = test(); tests_run++; \
+	printf("%sok %d - %s %s%s\n", (result ? "not " : ""), tests_run, message, result ? "# " : "", result ?: ""); \
+	if (result) return result; } while (0)
+
+#endif /* _MINTUNIT_H */
